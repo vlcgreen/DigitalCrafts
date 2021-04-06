e6eaf90503984ef62ae2e53a72058f84efc9a644
Victoria Green <victoria.cousan@gmail.com>
2021-04-06 14:17:56 -0500

    slow notes

---
+++ Python/Labs/py101.py
@@ -0,0 +1,54 @@
+# strings
+
+# "hello world"
+
+# 'hello world'
+
+# # \ escape character
+# 'I\'m'
+
+# "I'm"
+
+# # triple quotes several lines of text for string 
+
+# """
+# all
+# one
+# string
+# """
+# # print
+
+# print("Hello world")
+
+# print('abc')
+
+# print('cde')
+
+# print('Victoria' + ' Richards')
+
+# print('\tThis is a\n\t great day!')
+
+4
+5
+6
+# print(5 + 4)
+
+# print(5 * 5 * 5)
+# print(5 ** 2)
+
+# #only looking for remainders use %
+# print(5%2) ## 5/2 2*2 + 1 = 5
+
+# city = "Houston"
+
+# print(city)
+
+# print(city + " is a big city")
+
+found_coins = 20
+magic_coins = 10
+stolen_coins = 3
+
+result = found_coins + magic_coins * 365 - stolen_coins * 52
+
+print(result)
