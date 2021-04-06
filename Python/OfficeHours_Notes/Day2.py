7fba4f2018ea09d9fba6bec5b3bf81ab81bc8166
Victoria Green <victoria.cousan@gmail.com>
2021-04-06 16:28:32 -0500

    notes from after class

---
+++ Python/OfficeHours_Notes/Day2.py
@@ -0,0 +1,16 @@
+# Typecasting and variable types
+
+# age= int(input("How old are you?"))
+
+#Age is a string, we want it to be an integer
+
+# int()
+# print(age + 5)
+
+# isinstance()
+# Variable type: int, str, object
+
+# print(isinstance(5, int))
+
+# print(isinstance({}, object))  USEFUL FOR THE FUTURE
+
