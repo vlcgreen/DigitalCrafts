8f547b5b4d677424f1affe6965e346f7b2bed3a2
Victoria Green <victoria.cousan@gmail.com>
2021-04-06 15:21:59 -0500

    notes getting sparing

--- Python/Labs/py101.py
+++ Python/Labs/py101.py
@@ -45,10 +45,50 @@
 
 # print(city + " is a big city")
 
-found_coins = 20
-magic_coins = 10
-stolen_coins = 3
+# found_coins = 20
+# magic_coins = 10
+# stolen_coins = 3
 
-result = found_coins + magic_coins * 365 - stolen_coins * 52
+# result = found_coins + magic_coins * 365 - stolen_coins * 52
 
-print(result)
+# # print(result)
+
+first_name = "Victoria"
+last_name = "Richards"
+
+# # result = "Hello {1} {0} {0} {0}".format(first_name, last_name)
+# # result = "Hello {first} {last}".format(first=first_name, last=last_name)
+
+# result = f"Hello {first_name} {last_name}"
+
+# print(result)
+
+# print(type(first_name))
+# print(type(5))
+
+
+# print(isinstance(first_name, str))
+
+# name = input( 'What\'s your name? ')
+# last_name = input('What is your last name?')
+
+# print(f'hello {name} {last_name}')
+
+# print(type(name))
+# print(type(last_name))
+
+# four = '4'
+
+# print(type(four))
+
+# a= int(four)
+
+# print(type(a))
+
+# print(a * 4)
+
+# a = int(input('First value >>'))
+# b = int(input('Second value >>'))
+# c = a + b 
+# print(f'the value of {a} + {b} = {c}')
+
