68d62011c4bdf0c641fd14c27d87d54cb8cf432c
Victoria Green <victoria.cousan@gmail.com>
2021-04-07 16:51:02 -0500

    adding another assignment

--- Python/Homework/Day2HW_Problem3.py
+++ Python/Homework/Day2HW_Problem3.py
@@ -3,3 +3,16 @@
 # PROBLEM 3 - HOW MANY COINS?
 
 coins = 0
+
+while True:
+    print(f"\nYou currently have {coins}coins.")
+    answer = input("\nDo you want another? Y/N >> ")
+    if answer.lower() == 'y':
+        coins += 1
+    elif answer.lower() == 'n':
+        print("\nBye!")
+        break
+    else:
+        print("\nYou need to enter Y/N.")
+        continue
+
