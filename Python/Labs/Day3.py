fd8efa4f78be6b03cf09cdaa6babbb0ce88f75ed
Victoria Green <victoria.cousan@gmail.com>
2021-04-07 13:42:13 -0500

    adding my notes as I go

---
+++ Python/Labs/Day3.py
@@ -0,0 +1,18 @@
+#Day 3 ACTIVITIES
+
+day1 = "Sunday"
+day2 = "Monday"
+day3 = "Tuesday"
+day4 = "Wednesday"
+day5 = "Thursday"
+day6 = "Friday"
+day7 = "Saturday"
+#list
+##How do we acess an element
+## How do we delete
+## How do we insert
+## How do we update
+## CRUD
+days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'] 
+print(days)
+
