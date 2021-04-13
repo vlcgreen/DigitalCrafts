b2c9a32fcf48a5eb861db41ce1b0c0e802833134
Victoria Green <victoria.cousan@gmail.com>
2021-04-13 12:27:00 -0500

    phonebook lab files

---
+++ Python/Homework/Phonebook/Phonebook.py
@@ -0,0 +1,59 @@
+#Working on phonebook
+import pickle
+
+phonebook_dict = {
+    
+}
+
+#1. Look up an entry
+def LookUp(name):
+    with open('phonebook.pickle', 'rb') as fh:
+        phonebook_dict = pickle.load(fh)
+    print(f"Found entry for {name}: {phonebook_dict[name]}")
+#2. Set an entry
+def entry():
+    entry_name = input("\nWhat name do you want to enter? ")
+    entry_phone = input("What is their number? \n(Please format XXX-XXX-XXXX) >> ")
+    phonebook_dict[entry_name] = entry_phone
+    with open('phonebook.pickle', 'wb') as fh:
+        pickle.dump(phonebook_dict, fh)
+    print(f"Entry stored for: {phonebook_dict[entry_name]}")
+
+#3. Delete an entry
+def delete(name):
+    del phonebook_dict[name]
+    print(f"You have deleted the entry for {name}")
+
+#4. List all entries
+def all_entries():
+    print(phonebook_dict)
+
+# Ask user for input 
+## What do you want to do 1-5?
+action = " "
+while action != '1' or '2' or '3' or '4' or '5':
+        print('''
+        Electronic Phone Book
+        ======================
+        1. Look up an entry
+        2. Set an entry
+        3. Delete an entry
+        4. List all entries
+        5. Quit
+        ''')
+        action = input("\nWhat Would you like to do (1-5)?")
+
+        if action == '1':
+            name = input("\nWho would you like to look up? ")
+            LookUp(name)
+        if action == '2':
+            entry()
+        if action == '3':
+            name = input("\nWho would you like to delete? ")
+            delete(name)
+        if action == '4':
+            all_entries()
+        if action == '5':
+            print("\nBye.")
+            break
+
