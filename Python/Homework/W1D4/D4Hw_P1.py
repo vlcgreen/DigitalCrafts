350f2b63a0f0f01d01b520661611ce6b9ba74c57
Victoria Green <victoria.cousan@gmail.com>
2021-04-13 08:52:52 -0500

    more homework solves

--- Python/Homework/W1D4/D4Hw_P1.py
+++ Python/Homework/W1D4/D4Hw_P1.py
@@ -1,9 +1,24 @@
 #Functions Homework
 # Problem 1: Find the smallest number
 
-# def smallest(list1):
-#     index = 0
+list1 = []
 
-#     while index < len(smallest):
-#         if smallest[index] 
-        
+numbers = input("How many numbers do you want to enter?>> ")
+
+i1 = 0
+while i1 < numbers:
+    entered = int(input("Please enter a number for your list: "))
+    list1.append(entered)
+    i1 += 1 
+
+small = ""
+
+index = 0
+while index < len(list1):
+    small = list1[index]
+    if list1[index] < small:
+        small = list1[index]
+    index += 1 
+
+print (small)
+
