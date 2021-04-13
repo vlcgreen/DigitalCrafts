cb8a9b66b4baa6c13f16443822fa97715376fd52
Victoria Green <victoria.cousan@gmail.com>
2021-04-13 07:54:35 -0500

    fixed homework probs

--- Python/Homework/W1D3/D3HW_P5.py
+++ Python/Homework/W1D3/D3HW_P5.py
@@ -1,26 +1,21 @@
 #Leetspeak
 ##Given a paragraph of text as string, print the paragraph in leetspeak.
 
-### HAVING ISSUES FIGURING OUT THIS PROBLEM. 
+string = "I am a leet programmer"
+#if you want there to be input text see line 6
+#string = input("What would you like to say?>> ")
 
-# string = "I am a leet programmer"
-
-# A = '4'
-# E = '3'
-# G = '6'
-# I = '1'
-# O = '0'
-# S = '5'
-# T = '7'
-
-# code = list(string)
-# index = 0
-# while index < len(string):
-#     if code[index] == 'a':
-#         code[index] = '4'
-#         index += 1
-# print(code)
-
-##Having issues figuring this problem out.
-
-print('hello world')
+lstring = string.lower()
+index = 0
+while index < len(lstring):
+    lstring = lstring.replace("a", "4")
+    lstring = lstring.replace("e",'3')
+    lstring = lstring.replace("g",'6')
+    lstring = lstring.replace("i",'1')
+    lstring = lstring.replace("o",'0')
+    lstring = lstring.replace("s",'5')
+    lstring = lstring.replace("t",'7')
+    index += 1
+print(lstring)
+##part of me thinks I didn't need this in a loop
+##But since it took forever and it finally worked, I'll leave it!
---
+++ Python/Homework/W1D4/D4Hw_P1.py
@@ -0,0 +1,9 @@
+#Functions Homework
+# Problem 1: Find the smallest number
+
+# def smallest(list1):
+#     index = 0
+
+#     while index < len(smallest):
+#         if smallest[index] 
+        
